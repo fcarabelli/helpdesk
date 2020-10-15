caaf316d6887b33dcd8be86be33fe0bdc381da80
Ignacio Galvez <ignacio.galvez@alumnos.frm.utn.edu.ar>
2020-10-14 22:56:23 -0300

    added CaptchaField in QuestionForm

--- helpdeskapp/forms.py
+++ helpdeskapp/forms.py
@@ -1,8 +1,10 @@
+from captcha.fields import CaptchaField
 from django.forms import ModelForm
 from .models import Question
 
 
 class QuestionForm(ModelForm):
+    captcha = CaptchaField()
     class Meta:
         model = Question
         fields = ['subject','message','urgency']
