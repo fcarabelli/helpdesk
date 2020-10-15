6d0fc5e617f7915259a38c6fee557403bc8c5924
Ignacio Galvez <ignacio.galvez@alumnos.frm.utn.edu.ar>
2020-10-14 21:33:27 -0300

    modified fields of Question Model

--- helpdeskapp/models.py
+++ helpdeskapp/models.py
@@ -2,5 +2,6 @@ from django.db import models
 
 # Create your models here.
 class Question(models.Model):
-    question_text = models.CharField(max_length=1000)
+    subject = models.CharField(max_length=72,blank=False)
+    message = models.CharField(max_length=1000,blank=False)
     question_datetime = models.DateTimeField(auto_now=True)
