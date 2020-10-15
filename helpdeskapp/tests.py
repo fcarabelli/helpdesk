366c4710fd7562ae2c484c9855face8da0cc23c0
Ignacio Galvez <ignacio.galvez@alumnos.frm.utn.edu.ar>
2020-10-14 23:27:46 -0300

    created first QuestionTest

--- helpdeskapp/tests.py
+++ helpdeskapp/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+from helpdeskapp.models import Question
+
+from helpdeskapp.models import Question
+
+
+class QuestionTestCase(TestCase):
+    def setUp(self):
+        Question.objects.create(subject="Esto es una prueba",message="Esto es una prueba",urgency="HIGH")
+
+    def test_question_recently_created(self):
+        recent_creation = Question.objects.get(subject="Esto es una prueba")
+        self.assertEqual(recent_creation.message, 'Esto es una prueba')
+        self.assertEqual(recent_creation.urgency, 'HIGH')
