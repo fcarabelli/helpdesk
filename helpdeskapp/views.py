cc1c24f6041aec3edd0f06ae29ff5c9db3a44c3d
Federico Gallardo <fedegallar2006@hotmail.com>
2020-10-25 22:20:09 -0300

    Helpdeskapp now have template inheritance

--- helpdeskapp/views.py
+++ helpdeskapp/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+from helpdeskapp.forms import QuestionForm
+
+
+def addQuestion(request):
+    # We create an empty form
+    form = QuestionForm()
+
+    # We check if the form has been sent
+    if request.method == "POST":
+        # Añadimos los datos recibidos al formulario
+        form = QuestionForm(request.POST)
+        # If the form is valid ...
+        if form.is_valid():
+            # We save the form but without confirming it,
+            # so we will get an instance to handle it
+            instancia = form.save(commit=False)
+            # We can save it whenever we want
+            instancia.save()
+            # After saving we redirect to the list
+            return redirect('/')
+
+    # If we reach the end we render the form
+    return render(request, "helpdeskapp/addQuestion.html", {'form': form})
