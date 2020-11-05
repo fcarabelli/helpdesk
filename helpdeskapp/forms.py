49ce14a372661c8284ece4de08caffa30a268afd
nachogg18 <ignacio.galvez@alumnos.frm.utn.edu.ar>
2020-11-05 11:01:41 -0300

    deleted urgency field in models

--- helpdeskapp/forms.py
+++ helpdeskapp/forms.py
@@ -9,11 +9,10 @@ class QuestionForm(ModelForm):
     captcha = CaptchaField()
     class Meta:
         model = Question
-        fields = ['subject','message','urgency','email','phone_number']
+        fields = ['subject','message','email','phone_number']
         labels = {
             'subject': 'Asunto',
             'message': 'Mensaje',
-            'urgency': 'Urgencia',
             'email': 'mail del solicitante',
             'phone_number': 'celular'
         }
