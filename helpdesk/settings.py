94f2d2c3297755b0b33bc08804e40f1de247cf04
nachogg18 <44012745+nachogg18@users.noreply.github.com>
2020-12-13 16:23:33 -0300

    fixing ALLOWED_HOSTS

--- helpdesk/settings.py
+++ helpdesk/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','localhost','nameofapp.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1','localhost','helpdeskfrmutn.herokuapp.com']
 
 
 # Application definition
