f0297d0a66ad2877d562508692f67748880f29bc
Federico Gallardo <fedegallar2006@hotmail.com>
2021-04-10 12:33:12 -0300

    Now we don't store important data.

--- session/CustomSessionLogin/UTNAuth.py
+++ session/CustomSessionLogin/UTNAuth.py
@@ -1,7 +1,7 @@
 import mysql.connector
 import hashlib
 from django.conf import settings
-from django.contrib.auth.hashers import check_password
+from django.utils.crypto import get_random_string
 from ..models import User
 
 
@@ -11,33 +11,32 @@ class UTNAuth(object):
     """
 
     def authenticate(self, request, username, password, **kwargs):
+        m = hashlib.md5()
+        m.update(password.encode('utf-8'))
+        hashed_pass = m.hexdigest()
         try:
-            usuario = User.objects.get(email=username)
-            usuario_pass_valido = usuario.check_password(password)
-            if usuario_pass_valido and usuario.is_active:
-                print(usuario)
-                return usuario
+            db = mysql.connector.connect(**settings.UTN_CONFIG)
+            cursor = db.cursor()
+            query = "SELECT (goto), username, password2 FROM mailbox WHERE (goto) = %s AND password2 = %s"
+            cursor.execute(query, (username, hashed_pass))
+            user = cursor.fetchone()
+            if user is not None:
+                try:
+                    usuario = User.objects.get(email=user[0])
+                    return usuario
+                except User.DoesNotExist:
+                    full_name = str.split(user[1])
+                    first_name = full_name[0]
+                    last_name = full_name[1]
+                    usr = User.objects.create_user(username=user[0], email=user[0], first_name=first_name,
+                                                   last_name=last_name, cellphone=0)
+                    usr.set_password(get_random_string().upper())
+                    usr.save()
+                    return usr
             else:
                 return None
-        except User.DoesNotExist:
-            m = hashlib.md5()
-            m.update(password.encode('utf-8'))
-            hashed_pass = m.hexdigest()
-            try:
-                db = mysql.connector.connect(**settings.UTN_CONFIG)
-                cursor = db.cursor()
-                query = "SELECT (goto), username, password2 FROM mailbox WHERE (goto) = %s AND password2 = %s"
-                cursor.execute(query, (username, hashed_pass))
-                user = cursor.fetchone()
-                full_name = str.split(user[1])
-                first_name = full_name[0]
-                last_name = full_name[1]
-                usr = User.objects.create_user(username=user[0], email=user[0], first_name=first_name, last_name=last_name, cellphone=0)
-                usr.set_password(password)
-                usr.save()
-                return usr
-            except mysql.connector.Error as err:
-                return None
+        except:
+            return None
 
     def get_user(self, user_id):
         try:
