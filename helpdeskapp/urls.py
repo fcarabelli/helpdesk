8590de77fb54f88f86e57a343d64fe18b98ae055
Federico Gallardo <fedegallar2006@hotmail.com>
2021-04-11 13:17:49 -0300

    Index url added

--- helpdeskapp/urls.py
+++ helpdeskapp/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from helpdeskapp import views
 
 urlpatterns = [
+    path('', views.index, name='helpdeskapp-index'),
     path('addQuestion', views.addQuestion, name='addQuestion'),
 ]
